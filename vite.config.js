80666fb5409a7752012b35dacf50dbb66a1dc4da
OyenCar <kevinsetiadiwijaya@gmail.com>
2026-01-09 06:11:55 +0700

    Fix Salah nama

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/WebsiteBengkel/", // <-- Tambahkan ini (sesuai nama repo, pakai tanda slash)
+  base: "/BengkelWijaya/", // <-- Tambahkan ini (sesuai nama repo, pakai tanda slash)
 })
