0f3ee14dea22a2ef935f50c4f304f74118449373
OyenCar <kevinsetiadiwijaya@gmail.com>
2026-01-09 05:44:42 +0700

    Upload pertama Bengkel Wijaya

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/NAMA_REPOSITORY/", // <-- Tambahkan ini (sesuai nama repo, pakai tanda slash)
+  base: "/WebsiteBengkel/", // <-- Tambahkan ini (sesuai nama repo, pakai tanda slash)
 })
